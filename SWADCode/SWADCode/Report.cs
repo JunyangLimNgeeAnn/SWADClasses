fc0e7b046693fb8ff2f23ae034e6cf7db28a42ca
JunyangLimNgeeAnn <158800135+JunyangLimNgeeAnn@users.noreply.github.com>
2024-08-07 17:57:42 +0800

    Update Report.cs

--- SWADCode/SWADCode/Report.cs
+++ SWADCode/SWADCode/Report.cs
@@ -8,5 +8,20 @@ namespace SWADCode
 {
     internal class Report
     {
+        private string reportid;
+
+        public string ReportID { get; set; }
+
+        private string Typeofancident;
+
+        public string TypeOfAccident  {get; set; }
+
+       private string dateOfIncident;
+
+       public string DateOfIncident  {get; set; }
+
+      private string descriptionofaccident;
+
+     public string DescriptionOfIncident { get; set; }
     }
 }
